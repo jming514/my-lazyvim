14b621d10604c39d0679584bddda27427576091d
Lester Tang <junming@tjm-mbp.local>
2023-08-05 13:18:54 -0400

    change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,20 @@
 return {
-  { "rose-pine/neovim" },
-  { "catppuccin/nvim" },
+  -- { "rose-pine/neovim" },
+  -- { "catppuccin/nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "tokyonight",
     },
   },
 }
