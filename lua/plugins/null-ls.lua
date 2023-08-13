979291caba78bdd5594dd03d9bdfdcafae08e59b
jming514 <tang_lester@hotmail.com>
2023-08-13 00:14:11 -0400

    fix prettier not working on linux
    For some reason overriding the null-ls setup fixes this. Following the
    lazyvim configuration in the docs does not work. Use to only work after
    reinstalling prettierd every time.

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,11 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "mason.nvim" },
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    nls.setup({
+      -- debug = true
+    })
+    -- table.insert(opts.sources, nls.builtins.formatting.prettierd)
+  end,
+}
