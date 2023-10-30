418a92b21b7ef0d70a1b623204c7034c5d8ff4d7
Lester Tang <junming@tjm-mbp.local>
2023-10-30 14:23:19 -0400

    remove 100 width

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -3,9 +3,7 @@ return {
     "shortcuts/no-neck-pain.nvim",
     version = "*",
     config = function()
-      require("no-neck-pain").setup({
-        width = 100,
-      })
+      require("no-neck-pain").setup({})
     end,
   },
 }
