68026a1fceeefa969583d7b46b7f57868908dd68
Lester Tang <junming@tjm-mbp.local>
2023-12-17 17:04:39 -0500

    toggle inlay hints

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,8 +5,30 @@ return {
       -- autoformat = false,
       servers = {
         tsserver = {
-          on_attach = function(client)
+          on_attach = function(client, bufnr)
             client.server_capabilities.documentFormattingProvider = false
+
+            local function toggle_inlay_hints()
+              if vim.g.inlay_hints_visible then
+                vim.g.inlay_hints_visible = false
+                vim.lsp.inlay_hint.enable(bufnr, false)
+              else
+                if client.server_capabilities.inlayHintProvider then
+                  vim.g.inlay_hints_visible = true
+                  vim.lsp.inlay_hint.enable(bufnr, true)
+                else
+                  print("no inlay hints available")
+                end
+              end
+            end
+
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
+            vim.keymap.set(
+              "n",
+              "<leader>ih",
+              toggle_inlay_hints,
+              vim.tbl_extend("force", bufopts, { desc = "✨lsp toggle inlay hints" })
+            )
           end,
           init_options = {
             preferences = {
