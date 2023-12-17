4dcf0846cd917f2ecb31f7f72d2c4f0895aba18a
Lester Tang <junming@tjm-mbp.local>
2023-12-17 17:36:20 -0500

    fix on_attach inlay hints toggle

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,34 +1,40 @@
+local on_attach = function(client, bufnr)
+  local function toggle_inlay_hints()
+    if vim.g.inlay_hints_visible then
+      vim.g.inlay_hints_visible = false
+      vim.lsp.inlay_hint.enable(bufnr, false)
+    else
+      if client.server_capabilities.inlayHintProvider then
+        vim.g.inlay_hints_visible = true
+        vim.lsp.inlay_hint.enable(bufnr, true)
+      else
+        print("no inlay hints available")
+      end
+    end
+  end
+
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set(
+    "n",
+    "<leader>h",
+    toggle_inlay_hints,
+    vim.tbl_extend("force", bufopts, { desc = "✨lsp toggle inlay hints" })
+  )
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       -- autoformat = false,
       servers = {
+        gopls = {
+          on_attach = on_attach,
+        },
         tsserver = {
           on_attach = function(client, bufnr)
             client.server_capabilities.documentFormattingProvider = false
-
-            local function toggle_inlay_hints()
-              if vim.g.inlay_hints_visible then
-                vim.g.inlay_hints_visible = false
-                vim.lsp.inlay_hint.enable(bufnr, false)
-              else
-                if client.server_capabilities.inlayHintProvider then
-                  vim.g.inlay_hints_visible = true
-                  vim.lsp.inlay_hint.enable(bufnr, true)
-                else
-                  print("no inlay hints available")
-                end
-              end
-            end
-
-            local bufopts = { noremap = true, silent = true, buffer = bufnr }
-            vim.keymap.set(
-              "n",
-              "<leader>ih",
-              toggle_inlay_hints,
-              vim.tbl_extend("force", bufopts, { desc = "✨lsp toggle inlay hints" })
-            )
+            on_attach(client, bufnr)
           end,
           init_options = {
             preferences = {
