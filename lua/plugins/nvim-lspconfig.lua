e14b46f93f0fe3b3256ca120d5f3688e1d796e34
Lester Tang <junming@tjm-mbp.local>
2023-12-17 16:53:41 -0500

    inlay hints for tsserver

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,26 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       -- autoformat = false,
+      servers = {
+        tsserver = {
+          on_attach = function(client)
+            client.server_capabilities.documentFormattingProvider = false
+          end,
+          init_options = {
+            preferences = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+              importModuleSpecifierPreference = "non-relative",
+            },
+          },
+        },
+        biome = {},
+      },
       inlay_hints = {
         enabled = true,
       },
