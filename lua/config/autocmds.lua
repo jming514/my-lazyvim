a3513feb5f5704639bd9a142e3b35eeac34ee927
Lester Tang <junming@02-14-06-06.tbc.loc>
2023-12-29 08:38:35 -0500

    add .templ extension autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,5 +12,7 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
     vim.opt_local.expandtab = true
-  end
+  end,
 })
+
+vim.cmd([[autocmd BufRead,BufNewFile *.templ setfiletype templ]])
