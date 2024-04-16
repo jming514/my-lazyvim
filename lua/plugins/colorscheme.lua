b19fabc405808f0b0f43d4a1f63d473c97585d9d
Lester Tang <junming@tjm-mbp.local>
2024-04-15 23:16:13 -0400

    change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,28 @@
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   -- "jming514/clrs",
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       transparent_background = false,
+  --       -- flavour = "latte",
+  --     })
+  --   end,
+  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    -- "jming514/clrs",
+    "rebelot/kanagawa.nvim",
     config = function()
-      require("catppuccin").setup({
-        transparent_background = false,
-        -- flavour = "latte",
+      require("kanagawa").setup({
+        theme = "dragon",
       })
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
